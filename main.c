047a7cb08df1a3f47a420b17978091866b1a3bf0
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2025-05-29 13:11:28 -0300

    m

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+void autofantastico();
+void menu();
+
 int main() {
-  printf("Hello mom\n");
+  menu();
   return 0;
+}
+
+void menu() {
+  int opcion;
+  printf("Ingrese un numero\n");
+  while (1) {
+    scanf("%d", &opcion);
+    switch (opcion) {
+    case 1:
+      autofantastico();
+      return;
+    default:
+      printf("Ingrese un numero válido\n");
+    }
+  }
+}
+
+void autofantastico() {
+  printf("Autofantastico!\n");
+  return;
 }
